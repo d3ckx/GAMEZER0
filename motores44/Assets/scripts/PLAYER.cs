11f1a52498e9a2d6f17bb7aba66c0b4dd1e69905
d3ckx <ramonsouza142009@gmail.com>
2025-05-19 12:04:10 -0300

    13

--- motores44/Assets/scripts/PLAYER.cs
+++ motores44/Assets/scripts/PLAYER.cs
@@ -1,32 +1,33 @@
 using UnityEngine;
-using UnityEngine;
 
 public class PLAYER : MonoBehaviour
 {
-    private int numero;
+   
     public float velocidade = 0.7f;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        numero = 0;
+       
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
-        // Debug.Log(numero);
-        //numero = numero + 1;
+       
+            if (Input.GetKey(KeyCode.A))
+                gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
 
-        if (Input.GetKey(KeyCode.A))
-            gameObject.transform.position += new Vector3(-velocidade * Time.deltaTime, 0, 0);
+            if (Input.GetKey(KeyCode.D))
+                gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
 
-        if (Input.GetKey(KeyCode.D))
-            gameObject.transform.position += new Vector3(velocidade * Time.deltaTime, 0, 0);
+            if (Input.GetKey(KeyCode.S))
+                gameObject.transform.position += new Vector3(0, -velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
 
-        if (Input.GetKey(KeyCode.S))
-            gameObject.transform.position += new Vector3(0, -velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
-
-        if (Input.GetKey(KeyCode.W))
-            gameObject.transform.position += new Vector3(0, velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
+            if (Input.GetKey(KeyCode.W))
+                gameObject.transform.position += new Vector3(0, velocidade * Time.deltaTime, 0); // Corrigido a linha aqui
+        }
     }
-}
+        
+    
+
